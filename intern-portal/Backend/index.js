ee86878b7f3091892824978638bc4c4aa03aedff
ananya asa <ananyakayarthadka624@gmail.com>
2025-08-02 18:03:22 +0530

    fix: use dynamic port for deployment

--- intern-portal/Backend/index.js
+++ intern-portal/Backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app=express();
-const port=5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -31,6 +31,6 @@ app.get("/api/intern/:referralCode",(req,res)=>{
     }
 });
 
-app.listen(port,()=>{
+app.listen(PORT,()=>{
     console.log(`Server is running on port ${port}`);
 });
